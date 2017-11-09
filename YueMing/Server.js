2579c2dd3faddb6931fc06cd70a2f246b165bc43
Mingyue-Wuxie <627240400@qq.com>
2017-11-09 21:16:28 +0800

    服务器启动

---
+++ YueMing/Server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+const IP = '173.254.225.195';
+const port = 80;
+
+app.use(express.static(__dirname));
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/mess');
+
+const mess = new mongoose.Schema({
+    Company: { type: String },
+    Address: { type: String },
+    Mailbox: { type: String },
+    Phone: { type: Number },
+    Notes: { type: String },
+    Data: { type: Date, default: Date.now }
+});
+
+var message = mongoose.model('mess', mess);
+
+router.post('/set?*', (res, req) => {
+    message.create(res.query, function (err, data) {
+        if (err) {
+            req.end(err);
+            return;
+        }
+        console.log(data);
+    });
+    req.end('提交成功');
+});
+
+router.get('/get', (res, req) => {
+    message.find({}, (err, data) => {
+        if (err) {
+            req.end(err);
+            return;
+        }
+        req.json(data);
+    })
+});
+
+app.use(router);
+
+app.listen(port, IP);
+
+console.log(`${IP}:${port}`);
